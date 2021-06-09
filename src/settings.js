f3e7d6ed1fddc6fdb30b371da509f910a3bea6a5
er567 <839153198@qq.com>
2021-06-09 09:28:49 +0800

    init

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '原子市场监控',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
